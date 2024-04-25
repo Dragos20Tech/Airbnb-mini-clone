37e19ae298ebe11165906fca69f356ecf82cf724
Dragos20Tech <dragospetrescu5@gmail.com>
2024-04-25 13:16:23 +0300

    Updated card component to meet accessibility req

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,14 +5,14 @@ import star from "../assets/star.png"
 export function Card() {
     return (
         <div className="card">
-            <img src={KatieZaferes} className="card--image"  alt="Katie Zaferes"/>
+            <img src={KatieZaferes} className="card--image"  alt="main card image"/>
             <div className="card--stats">
-                <img src={star} className="card--star"  alt="star"/>
+                <img src={star} className="card--star"  alt="star icon"/>
                 <span>5.0</span>
                 <span className="gray">(6) • </span>
                 <span className="gray">USA</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
+            <h2>Life Lessons with Katie Zaferes</h2>
             <p><span className="bold">From $136</span> / person</p>
         </div>
     )
