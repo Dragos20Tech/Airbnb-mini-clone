9992654a105290c30a52321aca1af96089e6eeb8
Dragos20Tech <dragospetrescu5@gmail.com>
2024-04-29 10:14:26 +0300

    Added key prop

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import './App.css'
 function App() {
     const cards = data.map(item => {
         return <Card
+            key={item.id}
             img={item.coverImg}
             rating={item.stats.rating}
             reviewCount={item.stats.reviewCount}
