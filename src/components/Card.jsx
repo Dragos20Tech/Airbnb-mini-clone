620da445fbcf2a021aa2752560701e2c08ea0ff9
Dragos20Tech <dragospetrescu5@gmail.com>
2024-04-29 12:15:47 +0300

    Passed obj as props to refactor the mapping of card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,24 +3,24 @@ import star from "../../public/assets/star.png"
 
 export function Card(props) {
     let badgeText
-    if (props.openSpots === 0){
+    if (props.item.openSpots === 0){
         badgeText = "SOLD OUT"
     }
-    else if (props.location === "Online"){
+    else if (props.item.location === "Online"){
         badgeText = "ONLINE"
     }
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={props.img} className="card--image"  alt="main card image"/>
+            <img src={props.item.coverImg} className="card--image"  alt="main card image"/>
             <div className="card--stats">
                 <img src={star} className="card--star"  alt="star icon"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <h2>{props.title}</h2>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <h2>{props.item.title}</h2>
+            <p><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
