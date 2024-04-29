6ca9daa39c777cdca7e008e53ad15872aa6aa7a2
Dragos20Tech <dragospetrescu5@gmail.com>
2024-04-29 12:06:28 +0300

    Used conditional rendering to display the newly added badge for the card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,16 @@ import React from "react"
 import star from "../../public/assets/star.png"
 
 export function Card(props) {
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }
+    else if (props.location === "Online"){
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={props.img} className="card--image"  alt="main card image"/>
             <div className="card--stats">
                 <img src={star} className="card--star"  alt="star icon"/>
